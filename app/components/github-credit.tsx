25b7f0fe815625bb9940b14a1c4f35169594c3ed
RajadorDev <rajadordeveloper@gmail.com>
2026-05-18 14:13:51 -0300

    Added credit component

---
+++ app/components/github-credit.tsx
@@ -0,0 +1,11 @@
+
+import '@/app/css/github-credit.css';
+
+export default function GitHubCredit() {
+    return (
+        <a className="github-box" href="https://github.com/RajadorDev/">
+            <img src="img/github-icon.svg" alt="RajadorDeveloper github"/>
+            <p>Rajador Developer</p>
+        </a>
+    )
+}
